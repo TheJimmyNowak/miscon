db820f091b75131c4ccdb9b07780b3650730c383
miki164 <miki3867@gmail.com>
2019-10-28 16:45:16 +0100

    Add bootstrap

--- guests/forms.py
+++ guests/forms.py
@@ -2,13 +2,38 @@ from django import forms
 
 
 class AddGuestForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    city = forms.CharField(max_length=100)
-    age = forms.IntegerField()
-    document = forms.CharField(max_length=50)
+    first_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Imię'
+        }
+    ))
+    last_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Nazwisko'
+        }
+    ))
+    city = forms.CharField(max_length=100, label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Miasto'
+        }
+    ))
+    age = forms.IntegerField(label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Wiek'
+        }
+    ))
+    document = forms.CharField(max_length=50, label='', widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Dokument'
+        }
+    ))
 
-    guest_type = forms.ChoiceField(choices=[
+    guest_type = forms.ChoiceField(label='', choices=[
         ('participant', 'Uczestnik'),
         ('guest', 'Gość'),
         ('volunteer', 'Wolontariusz')]
--- guests/models.py
+++ guests/models.py
@@ -9,4 +9,4 @@ class Guest(models.Model):
     document = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return str(self.id)
